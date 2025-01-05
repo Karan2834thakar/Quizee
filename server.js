78eab2e01e5125e994646dad4c812976f9e28069
Karan Thakar <karanthakar2834@gmail.com>
2025-01-05 19:48:16 +0530

    message3

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // console.log('hello')
+import express from 'express'
+import path from 'path'
 const express = require('express');
 const path = require('path');
-// import express from 'express'
-// import path from 'path'
 const app = express();
 const PORT = process.env.PORT || 3000;
 
